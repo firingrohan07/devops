73f9ddeb0a069c535892b68b688804cdc1d22c33
Rohan <rsharma.rohan0709@gmail.com>
2024-02-07 11:06:21 +0530

    first commit

---
+++ longest_subsequence.cpp
@@ -0,0 +1,19 @@
+// // #include <iostream>
+// // using namespace std;
+// // int main() {
+// //     int arr[]={56,23,54,21,22,24,55};
+// //     int longg=0;
+// //     for(int i=0;i<(sizeof(arr)/sizeof(int));i++){
+// //         cout<<arr[i];
+// //         // int first=arr[0];
+// //         // if(arr[i+1])
+// //     }
+    
+// //     return 0;
+// // }
+// #include <iostream>
+
+// int main()
+// {
+//     std::cout << "Hello World" << std::endl;
+// }
